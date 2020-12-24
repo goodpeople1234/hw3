3359d33e24f82252ce462064fe03e51e51c9e889
goodpeople1234 <73989887+goodpeople1234@users.noreply.github.com>
2020-12-24 10:16:23 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        public void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(117, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(167, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "瀏覽pcap檔";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(20, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 59);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "瀏覽";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "封包檔|*.pcap";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7,
+            this.Column8,
+            this.Column9});
+            this.dataGridView1.Location = new System.Drawing.Point(0, 6);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 38;
+            this.dataGridView1.Size = new System.Drawing.Size(2543, 1440);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "編號";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "時間";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "來源MAC";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "目的地MAC";
+            this.Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "來源IP";
+            this.Column5.Name = "Column5";
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "目的地IP";
+            this.Column6.Name = "Column6";
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "通訊協定";
+            this.Column7.Name = "Column7";
+            // 
+            // Column8
+            // 
+            this.Column8.HeaderText = "來源PORT";
+            this.Column8.Name = "Column8";
+            // 
+            // Column9
+            // 
+            this.Column9.HeaderText = "目的地PORT";
+            this.Column9.Name = "Column9";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 70);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(2538, 1479);
+            this.tabControl1.TabIndex = 3;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Location = new System.Drawing.Point(8, 39);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(2522, 1432);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "詳細資料";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.dataGridView2);
+            this.tabPage2.Location = new System.Drawing.Point(8, 39);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(2522, 1432);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "統計資料";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2,
+            this.dataGridViewTextBoxColumn3});
+            this.dataGridView2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.dataGridView2.Location = new System.Drawing.Point(6, 0);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowTemplate.Height = 38;
+            this.dataGridView2.Size = new System.Drawing.Size(1903, 1226);
+            this.dataGridView2.TabIndex = 4;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.HeaderText = "來源IP";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.HeaderText = "目的的IP";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.HeaderText = "封包數量";
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1941, 1144);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "PCAP讀檔與統計程式";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column9;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        public System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,91 @@
+﻿using SharpPcap;using SharpPcap.LibPcap;using PacketDotNet;
+using System;using System.Collections.Generic;using System.ComponentModel;using System.Data;using System.Drawing;using System.Linq;using System.Text;using System.Windows.Forms;
+namespace WindowsFormsApplication1{
+    public partial class Form1 : Form{
+        public Form1(){InitializeComponent();}
+        private static int packetIndex = 0;
+        private void button1_Click(object sender, EventArgs e) {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK){
+                label1.Text = openFileDialog1.FileName;
+                dataGridView1.Rows.Clear();
+                dataGridView2.Rows.Clear();
+                packetIndex = 0;
+                ICaptureDevice device;
+                try{device = new CaptureFileReaderDevice(label1.Text);device.Open();}
+                catch (Exception e2){MessageBox.Show("Caught exception when opening file" + e2.ToString());return;}
+                device.OnPacketArrival += new PacketArrivalEventHandler(device_OnPacketArrival);
+                device.Capture();
+                device.Close();
+                 }
+            }
+        private void device_OnPacketArrival(object sender, CaptureEventArgs e){
+            if (e.Packet.LinkLayerType == PacketDotNet.LinkLayers.Ethernet) {
+                var packet = PacketDotNet.Packet.ParsePacket(e.Packet.LinkLayerType, e.Packet.Data);
+                var ethernetPacket = (PacketDotNet.EthernetPacket)packet;
+                var ipPacket = IpPacket.GetEncapsulated(packet);
+                try{
+                    for (int i = 0; i < dataGridView2.RowCount - 1; i++){
+                       if (dataGridView2.Rows[i].Cells[0].Value.ToString() == ipPacket.SourceAddress.ToString()&&dataGridView2.Rows[i].Cells[1].Value.ToString() == ipPacket.DestinationAddress.ToString()){
+                            dataGridView2.Rows[i].Cells[2].Value = Int32.Parse(dataGridView2.Rows[i].Cells[2].Value.ToString()) + 1;
+                            goto ok;
+                        }
+                    }
+                    dataGridView2.Rows.Add(new string[]{ipPacket.SourceAddress.ToString(), ipPacket.DestinationAddress.ToString(), "1" });
+                   }
+                catch { }
+                ok:
+                try{
+                    switch (ipPacket.Protocol){
+                        case IPProtocolType.TCP:
+                            var tcppacket = TcpPacket.GetEncapsulated(packet);
+                            dataGridView1.Rows.Add(new string[]{
+                                        packetIndex.ToString(),
+                                        e.Packet.Timeval.Date.ToString(),
+                                        ethernetPacket.SourceHwAddress.ToString(),
+                                        ethernetPacket.DestinationHwAddress.ToString(),
+                                        ipPacket.SourceAddress.ToString(),
+                                        ipPacket.DestinationAddress.ToString(),
+                                        ipPacket.Protocol.ToString(),
+                                        tcppacket.SourcePort.ToString(),
+                                        tcppacket.DestinationPort.ToString()
+                                     });
+                            break;
+                        case IPProtocolType.UDP:
+                            var udppacket = UdpPacket.GetEncapsulated(packet);
+                            dataGridView1.Rows.Add(new string[]{
+                                      packetIndex.ToString(),
+                                      e.Packet.Timeval.Date.ToString(),
+                                      ethernetPacket.SourceHwAddress.ToString(),
+                                      ethernetPacket.DestinationHwAddress.ToString(),
+                                      ipPacket.SourceAddress.ToString(),
+                                      ipPacket.DestinationAddress.ToString(),
+                                      ipPacket.Protocol.ToString(),
+                                      udppacket.SourcePort.ToString(),
+                                      udppacket.DestinationPort.ToString()
+                                     });
+                            break;
+                        default:
+                            dataGridView1.Rows.Add(new string[]{
+                                      packetIndex.ToString(),
+                                      e.Packet.Timeval.Date.ToString(),
+                                      ethernetPacket.SourceHwAddress.ToString(),
+                                      ethernetPacket.DestinationHwAddress.ToString(),
+                                      ipPacket.SourceAddress.ToString(),
+                                      ipPacket.DestinationAddress.ToString(),
+                                      ipPacket.Protocol.ToString()
+                                     });
+                            break;
+                    }
+                }
+                catch { dataGridView1.Rows.Add(new string[]{
+                                      packetIndex.ToString(),
+                                      e.Packet.Timeval.Date.ToString(),
+                                      ethernetPacket.SourceHwAddress.ToString(),
+                                      ethernetPacket.DestinationHwAddress.ToString(),
+                                      "","",ethernetPacket.Type.ToString()
+                                     });}
+                 packetIndex++;
+            }
+        }
+    }
+}
